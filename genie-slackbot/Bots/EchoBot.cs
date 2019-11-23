922c75886ac912c019f90fb754a794a9075f3c6d
MattMistele <matthew@mistele.com>
2019-11-23 14:21:34 -0800

    Make the genie functional. Will Smith knows what you're thinking

--- genie-slackbot/Bots/EchoBot.cs
+++ genie-slackbot/Bots/EchoBot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,7 +16,17 @@ namespace genie_slackbot.Bots
 
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+            AnswerOptions answer = (AnswerOptions)Enum.Parse(typeof(AnswerOptions), turnContext.Activity.Text);
+            var question = await client.Answer(answer);
+            if (client.GuessIsDue(question))
+            {
+                var guess = await client.GetGuess();
+                await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"My guess is: {guess[0].Name} - {guess[0].Description}"), cancellationToken);
+            }
+            else
+            {
+                AskQuestion(question, turnContext, cancellationToken);
+            }
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
@@ -24,11 +35,41 @@ namespace genie_slackbot.Bots
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    client = new AkinatorClient(Language.English, ServerType.Person);
-
                     await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"The genie's out of the bottle. Think of any person (real or fictional), and I'll guess it."), cancellationToken);
+
+                    // Start a new game and ask the first question
+                    client = new AkinatorClient(Language.English, ServerType.Person);
+                    var question = await client.StartNewGame();
+                    await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Question {question.Step + 1}: {question.Text}"), cancellationToken);
                 }
             }
+        }
+
+        private async void AskQuestion(AkinatorQuestion question, ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var options = GetOptions();
+            string possible = "Possible answers: ";
+            foreach (var option in options)
+            {
+                possible += option.Key;
+                possible += ": ";
+                possible += option.Value;
+                possible += " ";
+            }
+
+            await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Question {question.Step + 1}: {question.Text}"), cancellationToken);
+            await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak(possible), cancellationToken);
+        }
+
+        public Dictionary<int, string> GetOptions()
+        {
+            var myDic = new Dictionary<int, string>();
+            foreach (AnswerOptions foo in Enum.GetValues(typeof(AnswerOptions)))
+            {
+                myDic.Add((int)foo, foo.ToString());
+            }
+
+            return myDic;
         }
 
         private IActivity CreateActivityWithTextAndSpeak(string message)
